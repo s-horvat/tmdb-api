4119a2090b1bcb57c4edb253fadc25eb4dcc4189
Slaven Horvat <godric.v@gmail.com>
2024-11-08 21:34:12 +0100

    styled trending cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,21 +6,27 @@ function Card({ data, trending, index }) {
   const imgURL = useSelector((state) => state.tmdbData.imageURL);
 
   return (
-    <div className="w-full lg:text max-w-[250px]  overflow-hidden rounded h-100 relative">
+    <div className="w-full lg:text max-w-[250px]  overflow-hidden rounded-xl h-100 relative">
       <img src={imgURL + data?.poster_path} alt={data?.title || data?.name} />
 
-      <div className="absolute top-2">
+      <div className="absolute top-2 ">
         {trending && (
-          <div className="bg-black/60 backdrop-blur-3xl rounded-e-xl ps-2 pe-2 overflow-hidden">
-            <h2>
+          <div className="justify-between">
+            <h2 className="bg-black/70 backdrop-blur-3xl rounded-e-xl ps-2 pe-2 overflow-hidden">
               {index}. {data?.title || data?.name}
             </h2>
           </div>
         )}
       </div>
+
       <div>
-        <p className=" absolute w-full  backdrop-blur-3xl py-1 px-4 bottom-0 text-sm bg-black/60  text-center text-neutral-300">
-          {moment(data?.release_date).format("YYYY")}
+        <p className=" absolute w-full  backdrop-blur-3xl py-1 px-4 bottom-0 text-sm bg-black/60  flex justify-between text-neutral-300">
+          <span className="m-auto">
+            {moment(data?.release_date).format("YYYY")}{" "}
+          </span>
+          <span className="bg-yellow-700 p-0.5 rounded-full">
+            {Number(data?.vote_average).toFixed(1)}
+          </span>
         </p>
       </div>
     </div>
