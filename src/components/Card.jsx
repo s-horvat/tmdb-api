fc09074002224eb01d8ff1a4214dd6c431acf061
Slaven Horvat <godric.v@gmail.com>
2024-11-16 19:01:14 +0100

    tweeking scroll

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 function Card({ data, trending, index }) {
   const imgURL = useSelector((state) => state.tmdbData.imageURL);
 
   return (
-    <div className="w-full lg:text min-w-[230px] max-w-[250px]  overflow-hidden rounded-xl h-100 relative ">
+    <Link
+      to={"/" + data.media_type + "/" + data.id}
+      className="w-full lg:text min-w-[230px] max-w-[250px]  overflow-hidden rounded-xl h-100 relative z-10"
+    >
       <img src={imgURL + data?.poster_path} alt={data?.title || data?.name} />
 
       <div className="absolute top-2">
@@ -24,12 +28,12 @@ function Card({ data, trending, index }) {
           <span className="m-auto">
             {moment(data?.release_date).format("YYYY")}{" "}
           </span>
-          <span className="bg-yellow-700 p-0.5 rounded-full">
+          <span className="bg-yellow-700 p-0.5 text-black font-bold rounded-full">
             {Number(data?.vote_average).toFixed(1)}
           </span>
         </p>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/HorizontalScrollCard.jsx
+++ src/components/HorizontalScrollCard.jsx
@@ -21,10 +21,11 @@ function HorizontalScrollCard({ data = [], heading }) {
       <h2 className="text-xl lg:text-2xl font-bold text-white mb-2">
         Trending
       </h2>
+
       <div className="relative">
         <div
           ref={containerRef}
-          className="grid grid-cols-[repeat(20,230px)] gap-6 grid-flow-col overflow-auto scrollbar-none scroll-smooth overflow-x-scroll "
+          className="grid grid-cols-[repeat(20,230px)] gap-6 grid-flow-col overflow-auto scrollbar-none  scroll-smooth overflow-x-scroll "
         >
           {trendingData.map((data, index) => {
             return (
@@ -37,16 +38,16 @@ function HorizontalScrollCard({ data = [], heading }) {
             );
           })}
         </div>
-        <div className="absolute top-0 hidden h-full w-full lg:flex items-center justify-between px-2 ">
+        <div className="absolute top-0 h-full w-full hidden lg:flex items-center justify-between px-2 ">
           <button
             onClick={handePrevious}
-            className="bg-white p1 rounded-full text-3xl  text-black -ml-2"
+            className="bg-white p1 rounded-full text-3xl text-black -ml-2 z-20"
           >
             <FaAngleLeft />
           </button>
           <button
             onClick={handleNext}
-            className="bg-white p1 rounded-full text-3xl text-black -mr-2 "
+            className="bg-white p1 rounded-full text-3xl text-black -mr-2 z-20"
           >
             <FaAngleRight />
           </button>
