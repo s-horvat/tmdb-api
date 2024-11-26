d308e3aeeeeee600bd1067cf1ff058023c9cb0ff
Slaven Horvat <godric.v@gmail.com>
2024-11-26 23:00:02 +0100

    added cast and crew info

---
+++ src/components/Devider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Devider() {
+  return <div className="bg-neutral-700 p-[0.5px] rounded-full my-2"></div>;
+}
+
+export default Devider;
--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import useFetchDetails from "../hooks/useFetchDetails";
 import { useSelector } from "react-redux";
 import moment from "moment";
+import Devider from "../components/Devider";
 
 function DetailsPage() {
   const params = useParams();
@@ -12,8 +13,21 @@ function DetailsPage() {
     `/${params?.explore}/${params?.id}/credits`
   );
 
-  console.log("data", data);
-  console.log("credits", castData);
+  // console.log("data", data);
+  console.log("star cast", castData);
+  const writer = castData?.crew
+    ?.filter((el) => el?.job === "Writer")
+    ?.map((el) => el?.name)
+    ?.join(", ");
+  const director = castData?.crew
+    ?.filter((el) => el?.job === "Director")
+    ?.map((el) => el?.name)
+    ?.join(", ");
+  const producer = castData?.crew
+    ?.filter((el) => el?.job === "Producer")
+    ?.map((el) => el?.name)
+    ?.join(", ");
+  // console.log("writer", writer);
 
   return (
     <div>
@@ -27,28 +41,33 @@ function DetailsPage() {
           <div className="absolute w-full h-full top-0 "></div>
         </div>
       </div>
-      <div className="container mx-auto px-4 lg:py-0 py-16 flex hap-10 flex-col lg:flex-row gap-4">
-        <div className="relative mx-auto lg:-mt-28 rounded-lg lg:mx-0 flex">
+      <div className="container mx-auto px-4 lg:py-0 py-16 flex gap-10 flex-col lg:flex-row  ">
+        <div className="relative mx-auto lg:-mt-28 rounded-lg lg:mx-0 flex justify-evenly">
           <img
             src={imgURL + data?.poster_path}
             alt={data?.title || data?.nam}
-            className="lg:w-[26rem] w-60 object-cover rounded-xl lg:border-t-2 m-0 mt-3"
+            className="min-w-60 h-80 object-cover rounded-xl lg:border-t-2 m-0 mt-3 "
           />
         </div>
-        <div className=" pl-3 py-3 text-center">
-          <h1 className="font-bold text-2xl">{data?.title || data?.name}</h1>
-          <p className="text-center mb-5">{data?.tagline}</p>
+        <div className="pl-3 py-3 grid lg:grid-cols-[repeat(1,700px)] 2xl:grid-cols-[repeat(1,1000px)] justify-center gap-4 ">
+          <h1 className="font-bold text-center lg:text-start text-2xl">
+            {data?.title || data?.name}
+          </h1>
+          <p className="text-center lg:text-start mb-5">{data?.tagline}</p>
 
-          <div className="flex my-1 gap-5 justify-center">
-            <p>Rating: {Number(data?.vote_average).toFixed(1)}</p>
+          <Devider />
+
+          <div className="flex my-1 gap-5 justify-center lg:text-start">
+            <p className="lg:text-start">
+              Rating:{" "}
+              <span className="bg-yellow-700 p-0.5 text-black text- font-bold rounded-full ml-1">
+                {Number(data?.vote_average).toFixed(1)}
+              </span>
+            </p>
+            <span>|</span>
             <p>Duration: {data?.runtime} min</p>
-          </div>
-          <div>
-            <h3 className="text-xl font-bold">Overview:</h3>
-            <p>{data?.overview}</p>
-          </div>
-          <div>
-            <p className="mt-4 text-start">
+            <span>|</span>
+            <p>
               Release date:{" "}
               {moment(data?.first_air_date || data?.release_date).format(
                 "MMMM Do YYYY"
@@ -56,6 +75,43 @@ function DetailsPage() {
               .
             </p>
           </div>
+          <Devider />
+          <div className="flex flex-col">
+            <h3 className="text-xl font-bold pb-1">Overview:</h3>
+            <p>{data?.overview}</p>
+          </div>
+
+          <Devider />
+          <div>
+            <p>Director: {director}</p>
+            <p>Producer: {producer}</p>
+            <p>Writer: {writer}</p>
+          </div>
+        </div>
+      </div>
+      <div className="container w-full pl-10 ">
+        <span>
+          <Devider />
+        </span>
+        <h2 className="lg:text-3xl text-xl font-bold mb-10">Cast:</h2>
+        <div className="grid grid-cols-[repeat(auto-fit,96px)] justify-center gap-4">
+          {castData?.cast
+            ?.filter((el) => el?.profile_path)
+            .slice(0, 50)
+            .map((cast, index) => {
+              return (
+                <div key={index}>
+                  <div>
+                    <img
+                      src={imgURL + cast?.profile_path}
+                      alt={cast?.name}
+                      className="w-26 h-26 object-cover rounded-3xl "
+                    />
+                  </div>
+                  <p className="text-center">{cast?.name}</p>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
